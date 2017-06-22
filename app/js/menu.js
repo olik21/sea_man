9518daad57f2e013c881c0fbc5967ac0181f05b3
olik21 <kolga210921@gmail.com>
2017-06-22 14:05:48 +0300

    added menu, slider, team

---
+++ app/js/menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { 
+      $(".menu-js").click(function(){ 
+          
+        $(".ul-js").slideToggle(2000);  
+      
+      });
+    });
+  
+ 
--- app/js/slider.js
+++ app/js/slider.js
@@ -1,37 +1,10 @@
-
-var mySwiper = new Swiper ('#swiper-container1', {
  
-    loop: true,
-    
-    // If we need pagination
-    pagination: '.swiper-pagination',
-    paginationClickable: true
- 
- }); 
-
-  
-var mySwiper2 = new Swiper ('#swiper-container2', {
- 
-    loop: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    slidesPerView: 4,
-        spaceBetween: 20,
-        breakpoints: {
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            992: {
-                slidesPerView: 2,
-                spaceBetween: 10
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            }
-        }
-
- }); 
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30
+    });
   
  
